117a2cf8720a890bd26350f8570e8f8328fbfdb9
lvillarin <49315790+lilyvp1982@users.noreply.github.com>
2021-09-20 11:26:23 +0200

    msg parsing

--- main.js
+++ main.js
@@ -1,11 +1,31 @@
 const result = document.querySelector('.result');
+const error = document.querySelector('.error');
+
 
 if (window.Worker) {
   const myWorker = new Worker("worker.js");
 
 
   myWorker.onmessage = function(e) {
-    result.textContent = e.data;
+    const parsedMsg = e.data.split(":");
+
+    if (parsedMsg.length == 1)
+      parsedMsg[1] = parsedMsg[0];
+    const source = parsedMsg.shift();
+    const msg = (parsedMsg.length>1)? parsedMsg.join(":"): parsedMsg[0];
+
+    switch (source){
+      case "Error":
+        error.textContent = msg;
+        result.textContent = "";
+        break;
+      default:   
+        result.textContent = msg;
+        error.textContent = "";
+        break;
+    }
+    
+
     console.log('Message received from worker');
   }
 } else {
